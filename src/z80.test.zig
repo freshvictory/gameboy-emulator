8887a37d4725678a6e6251fcdfc9100668dbc9d2
Justin Renjilian <justin.renjilian@gmail.com>
2026-03-24 19:49:15 -0400

    Z80: fix function name style.

--- src/z80.test.zig
+++ src/z80.test.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Z80 = @import("z80.zig");
 const Flags = Z80.Flags;
 
-pub fn run_tests_for(comptime instruction: []const u8, expected_clock_m: usize) !void {
+pub fn runTestsFor(comptime instruction: []const u8, expected_clock_m: usize) !void {
     const json = @embedFile("sm83/v1/" ++ instruction ++ ".json");
     const parsed_test_cases = try std.json.parseFromSlice([]TestCase, std.testing.allocator, json, .{
         .ignore_unknown_fields = true,
@@ -138,513 +138,513 @@ const CpuState = struct {
 };
 
 test "0x00 nop" {
-    try run_tests_for("00", 1);
+    try runTestsFor("00", 1);
 }
 
 test "0x01 load bc, n16" {
-    try run_tests_for("01", 3);
+    try runTestsFor("01", 3);
 }
 
 test "0x02 load [bc], a" {
-    try run_tests_for("02", 2);
+    try runTestsFor("02", 2);
 }
 
 test "0x03 inc bc" {
-    try run_tests_for("03", 2);
+    try runTestsFor("03", 2);
 }
 
 test "0x04 inc b" {
-    try run_tests_for("04", 1);
+    try runTestsFor("04", 1);
 }
 
 test "0x05 dec b" {
-    try run_tests_for("05", 1);
+    try runTestsFor("05", 1);
 }
 
 test "0x06 ld b, n8" {
-    try run_tests_for("06", 2);
+    try runTestsFor("06", 2);
 }
 
 test "0x07 rcla" {
-    try run_tests_for("07", 1);
+    try runTestsFor("07", 1);
 }
 
 test "0x08 ld [a16], sp" {
-    try run_tests_for("0x08", 5);
+    try runTestsFor("0x08", 5);
 }
 
 test "0x09 add hl, bc" {
-    try run_tests_for("09", 2);
+    try runTestsFor("09", 2);
 }
 
 test "0x0A ld a, [bc]" {
-    try run_tests_for("0A", 2);
+    try runTestsFor("0A", 2);
 }
 
 test "0x0B dec bc" {
-    try run_tests_for("0B", 2);
+    try runTestsFor("0B", 2);
 }
 
 test "0x0C inc c" {
-    try run_tests_for("0C", 1);
+    try runTestsFor("0C", 1);
 }
 
 test "0x0D dec c" {
-    try run_tests_for("0D", 1);
+    try runTestsFor("0D", 1);
 }
 
 test "0x0E ld c, n8" {
-    try run_tests_for("0E", 2);
+    try runTestsFor("0E", 2);
 }
 
 test "0x0F rrca" {
-    try run_tests_for("0F", 1);
+    try runTestsFor("0F", 1);
 }
 
 test "0x40 ld b, b" {
-    try run_tests_for("40", 1);
+    try runTestsFor("40", 1);
 }
 
 test "0x41 ld b, c" {
-    try run_tests_for("41", 1);
+    try runTestsFor("41", 1);
 }
 
 test "0x42 ld b, d" {
-    try run_tests_for("42", 1);
+    try runTestsFor("42", 1);
 }
 
 test "0x43 ld b, e" {
-    try run_tests_for("43", 1);
+    try runTestsFor("43", 1);
 }
 
 test "0x44 ld b, h" {
-    try run_tests_for("44", 1);
+    try runTestsFor("44", 1);
 }
 
 test "0x45 ld b, l" {
-    try run_tests_for("45", 1);
+    try runTestsFor("45", 1);
 }
 
 test "0x46 ld b, [hl]" {
-    try run_tests_for("46", 2);
+    try runTestsFor("46", 2);
 }
 
 test "0x47 ld b, a" {
-    try run_tests_for("47", 1);
+    try runTestsFor("47", 1);
 }
 
 test "0x48 ld c, b" {
-    try run_tests_for("48", 1);
+    try runTestsFor("48", 1);
 }
 
 test "0x49 ld c, c" {
-    try run_tests_for("49", 1);
+    try runTestsFor("49", 1);
 }
 
 test "0x4A ld c, d" {
-    try run_tests_for("4A", 1);
+    try runTestsFor("4A", 1);
 }
 
 test "0x4B ld c, e" {
-    try run_tests_for("4B", 1);
+    try runTestsFor("4B", 1);
 }
 
 test "0x4C ld c, h" {
-    try run_tests_for("4C", 1);
+    try runTestsFor("4C", 1);
 }
 
 test "0x4D ld c, l" {
-    try run_tests_for("4D", 1);
+    try runTestsFor("4D", 1);
 }
 
 test "0x4E ld c, [hl]" {
-    try run_tests_for("4E", 2);
+    try runTestsFor("4E", 2);
 }
 
 test "0x4F ld c, a" {
-    try run_tests_for("4F", 1);
+    try runTestsFor("4F", 1);
 }
 
 test "0x50 ld d, b" {
-    try run_tests_for("50", 1);
+    try runTestsFor("50", 1);
 }
 
 test "0x51 ld d, c" {
-    try run_tests_for("51", 1);
+    try runTestsFor("51", 1);
 }
 
 test "0x52 ld d, d" {
-    try run_tests_for("52", 1);
+    try runTestsFor("52", 1);
 }
 
 test "0x53 ld d, e" {
-    try run_tests_for("53", 1);
+    try runTestsFor("53", 1);
 }
 
 test "0x54 ld d, h" {
-    try run_tests_for("54", 1);
+    try runTestsFor("54", 1);
 }
 
 test "0x55 ld d, l" {
-    try run_tests_for("55", 1);
+    try runTestsFor("55", 1);
 }
 
 test "0x56 ld d, [hl]" {
-    try run_tests_for("56", 2);
+    try runTestsFor("56", 2);
 }
 
 test "0x57 ld d, a" {
-    try run_tests_for("57", 1);
+    try runTestsFor("57", 1);
 }
 
 test "0x58 ld e, b" {
-    try run_tests_for("58", 1);
+    try runTestsFor("58", 1);
 }
 
 test "0x59 ld e, c" {
-    try run_tests_for("59", 1);
+    try runTestsFor("59", 1);
 }
 
 test "0x5A ld e, d" {
-    try run_tests_for("5A", 1);
+    try runTestsFor("5A", 1);
 }
 
 test "0x5B ld e, e" {
-    try run_tests_for("5B", 1);
+    try runTestsFor("5B", 1);
 }
 
 test "0x5C ld e, h" {
-    try run_tests_for("5C", 1);
+    try runTestsFor("5C", 1);
 }
 
 test "0x5D ld e, l" {
-    try run_tests_for("5D", 1);
+    try runTestsFor("5D", 1);
 }
 
 test "0x5E ld e, [hl]" {
-    try run_tests_for("5E", 2);
+    try runTestsFor("5E", 2);
 }
 
 test "0x5F ld e, a" {
-    try run_tests_for("5F", 1);
+    try runTestsFor("5F", 1);
 }
 
 test "0x60 ld h, b" {
-    try run_tests_for("60", 1);
+    try runTestsFor("60", 1);
 }
 
 test "0x61 ld h, c" {
-    try run_tests_for("61", 1);
+    try runTestsFor("61", 1);
 }
 
 test "0x62 ld h, d" {
-    try run_tests_for("62", 1);
+    try runTestsFor("62", 1);
 }
 
 test "0x63 ld h, e" {
-    try run_tests_for("63", 1);
+    try runTestsFor("63", 1);
 }
 
 test "0x64 ld h, h" {
-    try run_tests_for("64", 1);
+    try runTestsFor("64", 1);
 }
 
 test "0x65 ld h, l" {
-    try run_tests_for("65", 1);
+    try runTestsFor("65", 1);
 }
 
 test "0x66 ld h, [hl]" {
-    try run_tests_for("66", 2);
+    try runTestsFor("66", 2);
 }
 
 test "0x67 ld h, a" {
-    try run_tests_for("67", 1);
+    try runTestsFor("67", 1);
 }
 
 test "0x68 ld l, b" {
-    try run_tests_for("68", 1);
+    try runTestsFor("68", 1);
 }
 
 test "0x69 ld l, c" {
-    try run_tests_for("69", 1);
+    try runTestsFor("69", 1);
 }
 
 test "0x6A ld l, d" {
-    try run_tests_for("6A", 1);
+    try runTestsFor("6A", 1);
 }
 
 test "0x6B ld l, e" {
-    try run_tests_for("6B", 1);
+    try runTestsFor("6B", 1);
 }
 
 test "0x6C ld l, h" {
-    try run_tests_for("6C", 1);
+    try runTestsFor("6C", 1);
 }
 
 test "0x6D ld l, l" {
-    try run_tests_for("6D", 1);
+    try runTestsFor("6D", 1);
 }
 
 test "0x6E ld l, [hl]" {
-    try run_tests_for("6E", 2);
+    try runTestsFor("6E", 2);
 }
 
 test "0x6F ld l, a" {
-    try run_tests_for("6F", 1);
+    try runTestsFor("6F", 1);
 }
 
 test "0x70 ld [hl], b" {
-    try run_tests_for("70", 2);
+    try runTestsFor("70", 2);
 }
 
 test "0x71 ld [hl], c" {
-    try run_tests_for("71", 2);
+    try runTestsFor("71", 2);
 }
 
 test "0x72 ld [hl], d" {
-    try run_tests_for("72", 2);
+    try runTestsFor("72", 2);
 }
 
 test "0x73 ld [hl], e" {
-    try run_tests_for("73", 2);
+    try runTestsFor("73", 2);
 }
 
 test "0x74 ld [hl], h" {
-    try run_tests_for("74", 2);
+    try runTestsFor("74", 2);
 }
 
 test "0x75 ld [hl], l" {
-    try run_tests_for("75", 2);
+    try runTestsFor("75", 2);
 }
 
 test "0x76 halt" {
-    try run_tests_for("76", 1);
+    try runTestsFor("76", 1);
 }
 
 test "0x77 ld [hl], a" {
-    try run_tests_for("77", 2);
+    try runTestsFor("77", 2);
 }
 
 test "0x78 ld a, b" {
-    try run_tests_for("78", 1);
+    try runTestsFor("78", 1);
 }
 
 test "0x79 ld a, c" {
-    try run_tests_for("79", 1);
+    try runTestsFor("79", 1);
 }
 
 test "0x7A ld a, d" {
-    try run_tests_for("7A", 1);
+    try runTestsFor("7A", 1);
 }
 
 test "0x7B ld a, e" {
-    try run_tests_for("7B", 1);
+    try runTestsFor("7B", 1);
 }
 
 test "0x7C ld a, h" {
-    try run_tests_for("7C", 1);
+    try runTestsFor("7C", 1);
 }
 
 test "0x7D ld a, l" {
-    try run_tests_for("7D", 1);
+    try runTestsFor("7D", 1);
 }
 
 test "0x7E ld a, [hl]" {
-    try run_tests_for("7E", 2);
+    try runTestsFor("7E", 2);
 }
 
 test "0x7F ld a, a" {
-    try run_tests_for("7F", 1);
+    try runTestsFor("7F", 1);
 }
 
 test "0x80 add a, b" {
-    try run_tests_for("80", 1);
+    try runTestsFor("80", 1);
 }
 
 test "0x81 add a, c" {
-    try run_tests_for("81", 1);
+    try runTestsFor("81", 1);
 }
 
 test "0x82 add a, d" {
-    try run_tests_for("82", 1);
+    try runTestsFor("82", 1);
 }
 
 test "0x83 add a, e" {
-    try run_tests_for("83", 1);
+    try runTestsFor("83", 1);
 }
 
 test "0x84 add a, h" {
-    try run_tests_for("84", 1);
+    try runTestsFor("84", 1);
 }
 
 test "0x85 add a, l" {
-    try run_tests_for("85", 1);
+    try runTestsFor("85", 1);
 }
 
 test "0x86 add a, [hl]" {
-    try run_tests_for("86", 2);
+    try runTestsFor("86", 2);
 }
 
 test "0x87 add a, a" {
-    try run_tests_for("87", 1);
+    try runTestsFor("87", 1);
 }
 
 test "0x90 sub a, b" {
-    try run_tests_for("90", 1);
+    try runTestsFor("90", 1);
 }
 
 test "0x91 sub a, c" {
-    try run_tests_for("91", 1);
+    try runTestsFor("91", 1);
 }
 
 test "0x92 sub a, d" {
-    try run_tests_for("92", 1);
+    try runTestsFor("92", 1);
 }
 
 test "0x93 sub a, e" {
-    try run_tests_for("93", 1);
+    try runTestsFor("93", 1);
 }
 
 test "0x94 sub a, h" {
-    try run_tests_for("94", 1);
+    try runTestsFor("94", 1);
 }
 
 test "0x95 sub a, l" {
-    try run_tests_for("95", 1);
+    try runTestsFor("95", 1);
 }
 
 test "0x96 sub a, [hl]" {
-    try run_tests_for("96", 2);
+    try runTestsFor("96", 2);
 }
 
 test "0x97 sub a, a" {
-    try run_tests_for("97", 1);
+    try runTestsFor("97", 1);
 }
 
 test "0xA0 and a, b" {
-    try run_tests_for("A0", 1);
+    try runTestsFor("A0", 1);
 }
 
 test "0xA1 and a, c" {
-    try run_tests_for("A1", 1);
+    try runTestsFor("A1", 1);
 }
 
 test "0xA2 and a, d" {
-    try run_tests_for("A2", 1);
+    try runTestsFor("A2", 1);
 }
 
 test "0xA3 and a, e" {
-    try run_tests_for("A3", 1);
+    try runTestsFor("A3", 1);
 }
 
 test "0xA4 and a, h" {
-    try run_tests_for("A4", 1);
+    try runTestsFor("A4", 1);
 }
 
 test "0xA5 and a, l" {
-    try run_tests_for("A5", 1);
+    try runTestsFor("A5", 1);
 }
 
 test "0xA6 and a, [hl]" {
-    try run_tests_for("A6", 2);
+    try runTestsFor("A6", 2);
 }
 
 test "0xA7 and a, a" {
-    try run_tests_for("A7", 1);
+    try runTestsFor("A7", 1);
 }
 
 test "0xA8 xor a, b" {
-    try run_tests_for("A8", 1);
+    try runTestsFor("A8", 1);
 }
 
 test "0xA9 xor a, c" {
-    try run_tests_for("A9", 1);
+    try runTestsFor("A9", 1);
 }
 
 test "0xAA xor a, d" {
-    try run_tests_for("AA", 1);
+    try runTestsFor("AA", 1);
 }
 
 test "0xAB xor a, e" {
-    try run_tests_for("AB", 1);
+    try runTestsFor("AB", 1);
 }
 
 test "0xAC xor a, h" {
-    try run_tests_for("AC", 1);
+    try runTestsFor("AC", 1);
 }
 
 test "0xAD xor a, l" {
-    try run_tests_for("AD", 1);
+    try runTestsFor("AD", 1);
 }
 
 test "0xAE xor a, [hl]" {
-    try run_tests_for("AE", 2);
+    try runTestsFor("AE", 2);
 }
 
 test "0xAF xor a, a" {
-    try run_tests_for("AF", 1);
+    try runTestsFor("AF", 1);
 }
 
 test "0xB0 or a, b" {
-    try run_tests_for("B0", 1);
+    try runTestsFor("B0", 1);
 }
 
 test "0xB1 or a, c" {
-    try run_tests_for("B1", 1);
+    try runTestsFor("B1", 1);
 }
 
 test "0xB2 or a, d" {
-    try run_tests_for("B2", 1);
+    try runTestsFor("B2", 1);
 }
 
 test "0xB3 or a, e" {
-    try run_tests_for("B3", 1);
+    try runTestsFor("B3", 1);
 }
 
 test "0xB4 or a, h" {
-    try run_tests_for("B4", 1);
+    try runTestsFor("B4", 1);
 }
 
 test "0xB5 or a, l" {
-    try run_tests_for("B5", 1);
+    try runTestsFor("B5", 1);
 }
 
 test "0xB6 or a, [hl]" {
-    try run_tests_for("B6", 2);
+    try runTestsFor("B6", 2);
 }
 
 test "0xB7 or a, a" {
-    try run_tests_for("B7", 1);
+    try runTestsFor("B7", 1);
 }
 
 test "0xB8 cp a, b" {
-    try run_tests_for("B8", 1);
+    try runTestsFor("B8", 1);
 }
 
 test "0xB9 cp a, c" {
-    try run_tests_for("B9", 1);
+    try runTestsFor("B9", 1);
 }
 
 test "0xBA cp a, d" {
-    try run_tests_for("BA", 1);
+    try runTestsFor("BA", 1);
 }
 
 test "0xBB cp a, e" {
-    try run_tests_for("BB", 1);
+    try runTestsFor("BB", 1);
 }
 
 test "0xBC cp a, h" {
-    try run_tests_for("BC", 1);
+    try runTestsFor("BC", 1);
 }
 
 test "0xBD cp a, l" {
-    try run_tests_for("BD", 1);
+    try runTestsFor("BD", 1);
 }
 
 test "0xBE cp a, [hl]" {
-    try run_tests_for("BE", 2);
+    try runTestsFor("BE", 2);
 }
 
 test "0xBF cp a, a" {
-    try run_tests_for("BF", 1);
+    try runTestsFor("BF", 1);
 }
--- src/z80.zig
+++ src/z80.zig
@@ -41,7 +41,7 @@ pub const Registers = struct {
 
     stack_pointer: u16 = 0,
 
-    pub fn set_16(self: *Registers, r: Register16, high: u8, low: u8) void {
+    pub fn set16(self: *Registers, r: Register16, high: u8, low: u8) void {
         switch (r) {
             .bc => {
                 self.b = high;
@@ -56,21 +56,21 @@ pub const Registers = struct {
                 self.l = low;
             },
             .sp => {
-                self.stack_pointer = to_u16(high, low);
+                self.stack_pointer = to16(high, low);
             },
         }
     }
 
-    pub fn read_16(self: Registers, r: Register16) u16 {
+    pub fn read16(self: Registers, r: Register16) u16 {
         return switch (r) {
-            .bc => to_u16(self.b, self.c),
-            .de => to_u16(self.d, self.e),
-            .hl => to_u16(self.h, self.l),
+            .bc => to16(self.b, self.c),
+            .de => to16(self.d, self.e),
+            .hl => to16(self.h, self.l),
             .sp => self.stack_pointer,
         };
     }
 
-    inline fn to_u16(high: u8, low: u8) u16 {
+    inline fn to16(high: u8, low: u8) u16 {
         return (@as(u16, high) << 8) | low;
     }
 };
@@ -107,7 +107,7 @@ pub fn init(registers: Registers) Z80 {
 }
 
 pub fn step(z80: *Z80) void {
-    const opcode = z80.read_byte(z80.program_counter);
+    const opcode = z80.readByte(z80.program_counter);
     z80.program_counter +%= 1;
     z80.operate(opcode);
 }
@@ -116,15 +116,15 @@ fn operate(z80: *Z80, opcode: u8) void {
     switch (opcode) {
         0x00 => {},
 
-        0x01 => z80.load_16(.bc),
-        0x11 => z80.load_16(.de),
-        0x21 => z80.load_16(.hl),
-        0x31 => z80.load_16(.sp),
+        0x01 => z80.load16(.bc),
+        0x11 => z80.load16(.de),
+        0x21 => z80.load16(.hl),
+        0x31 => z80.load16(.sp),
 
-        0x02 => z80.load_memory(.bc, z80.registers.a),
-        0x12 => z80.load_memory(.de, z80.registers.a),
-        0x22 => z80.load_memory(.bc, z80.registers.a),
-        0x32 => z80.load_memory(.bc, z80.registers.a),
+        0x02 => z80.loadMemory(.bc, z80.registers.a),
+        0x12 => z80.loadMemory(.de, z80.registers.a),
+        0x22 => z80.loadMemory(.bc, z80.registers.a),
+        0x32 => z80.loadMemory(.bc, z80.registers.a),
 
         0x3C => z80.increment("a"),
         0x04 => z80.increment("b"),
@@ -133,7 +133,7 @@ fn operate(z80: *Z80, opcode: u8) void {
         0x1C => z80.increment("e"),
         0x24 => z80.increment("h"),
         0x2C => z80.increment("l"),
-        0x34 => z80.increment_hl(),
+        0x34 => z80.incrementHL(),
 
         0x3D => z80.decrement("a"),
         0x05 => z80.decrement("b"),
@@ -142,38 +142,38 @@ fn operate(z80: *Z80, opcode: u8) void {
         0x1D => z80.decrement("e"),
         0x25 => z80.decrement("h"),
         0x2D => z80.decrement("l"),
-        0x35 => z80.decrement_hl(),
+        0x35 => z80.decrementHL(),
 
-        0x78...0x7F, 0x3E => z80.load_8("a", z80.operand_8(opcode)),
-        0x40...0x47, 0x06 => z80.load_8("b", z80.operand_8(opcode)),
-        0x48...0x4F, 0x0E => z80.load_8("c", z80.operand_8(opcode)),
-        0x50...0x57, 0x16 => z80.load_8("d", z80.operand_8(opcode)),
-        0x58...0x5F, 0x1E => z80.load_8("e", z80.operand_8(opcode)),
-        0x60...0x67, 0x26 => z80.load_8("h", z80.operand_8(opcode)),
-        0x68...0x6F, 0x2E => z80.load_8("l", z80.operand_8(opcode)),
-        0x70...0x75, 0x77, 0x36 => z80.load_memory(.hl, z80.operand_8(opcode)),
+        0x78...0x7F, 0x3E => z80.load8("a", z80.operand8(opcode)),
+        0x40...0x47, 0x06 => z80.load8("b", z80.operand8(opcode)),
+        0x48...0x4F, 0x0E => z80.load8("c", z80.operand8(opcode)),
+        0x50...0x57, 0x16 => z80.load8("d", z80.operand8(opcode)),
+        0x58...0x5F, 0x1E => z80.load8("e", z80.operand8(opcode)),
+        0x60...0x67, 0x26 => z80.load8("h", z80.operand8(opcode)),
+        0x68...0x6F, 0x2E => z80.load8("l", z80.operand8(opcode)),
+        0x70...0x75, 0x77, 0x36 => z80.loadMemory(.hl, z80.operand8(opcode)),
 
         0x76 => z80.halt(),
 
-        0x80...0x87, 0xC6 => z80.add_8(z80.operand_8(opcode)),
-        0x88...0x8F, 0xCE => z80.add_with_carry_8(z80.operand_8(opcode)),
-        0x90...0x97, 0xD6 => z80.subtract_8(z80.operand_8(opcode)),
-        0x98...0x9F, 0xDE => z80.subtract_with_carry_8(z80.operand_8(opcode)),
-        0xA0...0xA7, 0xE6 => z80.and_(z80.operand_8(opcode)),
-        0xA8...0xAF, 0xEE => z80.xor(z80.operand_8(opcode)),
-        0xB0...0xB7, 0xF6 => z80.or_(z80.operand_8(opcode)),
-        0xB8...0xBF, 0xFE => z80.compare_8(z80.operand_8(opcode)),
+        0x80...0x87, 0xC6 => z80.add(z80.operand8(opcode)),
+        0x88...0x8F, 0xCE => z80.addWithCarry(z80.operand8(opcode)),
+        0x90...0x97, 0xD6 => z80.subtract(z80.operand8(opcode)),
+        0x98...0x9F, 0xDE => z80.subtractWithCarry(z80.operand8(opcode)),
+        0xA0...0xA7, 0xE6 => z80.@"and"(z80.operand8(opcode)),
+        0xA8...0xAF, 0xEE => z80.xor(z80.operand8(opcode)),
+        0xB0...0xB7, 0xF6 => z80.@"or"(z80.operand8(opcode)),
+        0xB8...0xBF, 0xFE => z80.compare(z80.operand8(opcode)),
 
         else => {},
     }
 }
 
-fn write_byte(z80: *Z80, address: u16, value: u8) void {
+fn writeByte(z80: *Z80, address: u16, value: u8) void {
     z80.clock.tick();
     z80.mmu[address] = value;
 }
 
-fn read_byte(z80: *Z80, address: u16) u8 {
+fn readByte(z80: *Z80, address: u16) u8 {
     z80.clock.tick();
     return z80.mmu[address];
 }
@@ -182,7 +182,7 @@ fn read_byte(z80: *Z80, address: u16) u8 {
 /// This is valid for the 0x40...0xBF chunk that use registers
 /// and the byte pointed to by HL
 /// as well as the constant-based 0x_6 and 0x_E opcodes.
-fn operand_8(z80: *Z80, opcode: u8) u8 {
+fn operand8(z80: *Z80, opcode: u8) u8 {
     // The lower nibble, repeated twice as there are 8
     return switch ((opcode & 0x0F) % 0x08) {
         0x7 => z80.registers.a,
@@ -193,16 +193,16 @@ fn operand_8(z80: *Z80, opcode: u8) u8 {
         0x4 => z80.registers.h,
         0x5 => z80.registers.l,
         0x6 => switch (opcode) {
-            0x40...0xBF => z80.read_byte(z80.registers.read_16(.hl)),
-            else => z80.constant_8(),
+            0x40...0xBF => z80.readByte(z80.registers.read16(.hl)),
+            else => z80.constant8(),
         },
         else => unreachable,
     };
 }
 
 /// Get the 8 bit value at the program counter
-fn constant_8(z80: *Z80) u8 {
-    const value = z80.read_byte(z80.program_counter);
+fn constant8(z80: *Z80) u8 {
+    const value = z80.readByte(z80.program_counter);
     z80.program_counter +%= 1;
     return value;
 }
@@ -213,21 +213,21 @@ fn halt(z80: *Z80) void {
 }
 
 /// Load the value into the given register
-fn load_8(z80: *Z80, comptime register: []const u8, operand: u8) void {
+fn load8(z80: *Z80, comptime register: []const u8, operand: u8) void {
     @field(z80.registers, register) = operand;
 }
 
 /// Load the value into the byte pointed to by register
-fn load_memory(z80: *Z80, register: Register16, operand: u8) void {
-    z80.write_byte(z80.registers.read_16(register), operand);
+fn loadMemory(z80: *Z80, register: Register16, operand: u8) void {
+    z80.writeByte(z80.registers.read16(register), operand);
 }
 
 /// Load the value into the given registers
-fn load_16(z80: *Z80, register: Register16) void {
-    const low = z80.constant_8();
-    const high = z80.constant_8();
+fn load16(z80: *Z80, register: Register16) void {
+    const low = z80.constant8();
+    const high = z80.constant8();
 
-    z80.registers.set_16(register, high, low);
+    z80.registers.set16(register, high, low);
 }
 
 /// Increment the register by 1
@@ -255,8 +255,8 @@ fn decrement(z80: *Z80, comptime register: []const u8) void {
     };
 }
 
-fn increment_hl(z80: *Z80) void {
-    var value = z80.read_byte(z80.registers.read_16(.hl));
+fn incrementHL(z80: *Z80) void {
+    var value = z80.readByte(z80.registers.read16(.hl));
 
     value +%= 1;
 
@@ -264,11 +264,11 @@ fn increment_hl(z80: *Z80) void {
         .was_zero = value == 0,
     };
 
-    z80.write_byte(z80.registers.read_16(.hl), value);
+    z80.writeByte(z80.registers.read16(.hl), value);
 }
 
-fn decrement_hl(z80: *Z80) void {
-    var value = z80.read_byte(z80.registers.read_16(.hl));
+fn decrementHL(z80: *Z80) void {
+    var value = z80.readByte(z80.registers.read16(.hl));
 
     value -%= 1;
 
@@ -277,12 +277,12 @@ fn decrement_hl(z80: *Z80) void {
         .subtracted = true,
     };
 
-    z80.write_byte(z80.registers.read_16(.hl), value);
+    z80.writeByte(z80.registers.read16(.hl), value);
 }
 
 /// Add value to A
 /// Put result in A
-fn add_8(z80: *Z80, operand: u8) void {
+fn add(z80: *Z80, operand: u8) void {
     const old = z80.registers.a;
     z80.registers.a, const overflowed = @addWithOverflow(z80.registers.a, operand);
 
@@ -294,14 +294,14 @@ fn add_8(z80: *Z80, operand: u8) void {
 }
 
 /// TODO
-fn add_with_carry_8(z80: *Z80, operand: u8) void {
+fn addWithCarry(z80: *Z80, operand: u8) void {
     _ = operand;
     _ = z80;
 }
 
 /// Subtract value from A
 /// Put result in A
-fn subtract_8(z80: *Z80, operand: u8) void {
+fn subtract(z80: *Z80, operand: u8) void {
     const old = z80.registers.a;
     z80.registers.a, const underflowed = @subWithOverflow(z80.registers.a, operand);
 
@@ -314,13 +314,13 @@ fn subtract_8(z80: *Z80, operand: u8) void {
 }
 
 /// TODO
-fn subtract_with_carry_8(z80: *Z80, operand: u8) void {
+fn subtractWithCarry(z80: *Z80, operand: u8) void {
     _ = operand;
     _ = z80;
 }
 
 /// Bitwise and the value with A
-fn and_(z80: *Z80, operand: u8) void {
+fn @"and"(z80: *Z80, operand: u8) void {
     z80.registers.a &= operand;
 
     z80.flags = .{
@@ -330,7 +330,7 @@ fn and_(z80: *Z80, operand: u8) void {
 }
 
 /// Bitwise or the value with A
-fn or_(z80: *Z80, operand: u8) void {
+fn @"or"(z80: *Z80, operand: u8) void {
     z80.registers.a |= operand;
 
     z80.flags = .{
@@ -350,7 +350,7 @@ fn xor(z80: *Z80, operand: u8) void {
 /// Compare value to A
 /// If equal, flag zero
 /// If register > A, flag carry
-pub fn compare_8(z80: *Z80, operand: u8) void {
+pub fn compare(z80: *Z80, operand: u8) void {
     const old = z80.registers.a;
     const result, const underflowed = @subWithOverflow(z80.registers.a, operand);
 
