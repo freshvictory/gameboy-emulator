81efcec0bf8e03fa77d7de92104eb6bbbd385e4a
Justin Renjilian <justin.renjilian@gmail.com>
2026-04-03 22:51:03 -0400

    MMU: map serial writes to a Writer if provided.
    Blargg tests use serial writes as stdout.

--- src/mmu.zig
+++ src/mmu.zig
@@ -5,8 +5,17 @@ const MMU = @This();
 cartridge: Cartridge,
 internal: [0x10000]u8 = [_]u8{0} ** 0x10000,
 
+serial_writer: ?*std.io.Writer = null,
+
 pub fn init(cartridge: Cartridge) MMU {
     return .{ .cartridge = cartridge };
+}
+
+pub fn initWithWriter(cartridge: Cartridge, writer: *std.io.Writer) MMU {
+    return .{
+        .cartridge = cartridge,
+        .serial_writer = writer,
+    };
 }
 
 pub fn readByte(mmu: MMU, address: u16) u8 {
@@ -19,6 +28,12 @@ pub fn readByte(mmu: MMU, address: u16) u8 {
 pub fn writeByte(mmu: *MMU, address: u16, value: u8) void {
     switch (address) {
         0x0000...0x7FFF, 0xA000...0xBFFF => mmu.cartridge.writeByte(address, value),
+        0xFF01 => {
+            if (mmu.serial_writer) |writer| {
+                writer.printAsciiChar(value, .{}) catch {};
+            }
+            mmu.internal[address] = value;
+        },
         else => mmu.internal[address] = value,
     }
 }
