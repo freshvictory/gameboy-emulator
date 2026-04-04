7609c706bf108bd1f13313459a93dc48db41ec57
Justin Renjilian <justin.renjilian@gmail.com>
2026-04-04 10:05:24 -0400

    MMU: use discarding writer if none provided.

--- src/mmu.zig
+++ src/mmu.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 const Cartridge = @import("cartridge.zig");
 const MMU = @This();
 
+var null_writer = std.io.Writer.Discarding.init(&.{});
+
 cartridge: Cartridge,
 internal: [0x10000]u8 = [_]u8{0} ** 0x10000,
 
-serial_writer: ?*std.io.Writer = null,
+serial_writer: *std.io.Writer = &null_writer.writer,
 
 pub fn init(cartridge: Cartridge) MMU {
     return .{ .cartridge = cartridge };
@@ -29,9 +31,7 @@ pub fn writeByte(mmu: *MMU, address: u16, value: u8) void {
     switch (address) {
         0x0000...0x7FFF, 0xA000...0xBFFF => mmu.cartridge.writeByte(address, value),
         0xFF01 => {
-            if (mmu.serial_writer) |writer| {
-                writer.printAsciiChar(value, .{}) catch {};
-            }
+            mmu.serial_writer.printAsciiChar(value, .{}) catch {};
             mmu.internal[address] = value;
         },
         else => mmu.internal[address] = value,
