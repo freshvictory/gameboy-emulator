9cd97d218377767a720cf8a3877523679156f851
Justin Renjilian <justin.renjilian@gmail.com>
2026-04-05 10:44:22 -0400

    Web: add basic form for booting with file or test rom.

--- src/web_assembly.zig
+++ src/web_assembly.zig
@@ -4,9 +4,21 @@ const Gameboy = @import("root.zig");
 
 extern fn print(clock: u8) void;
 
+const MAX_CARTRIDGE_LENGTH = 8 * 1024 * 1024;
+var cartridge_buffer: [MAX_CARTRIDGE_LENGTH]u8 = undefined;
+
+export fn getCartridgeBufferPointer() [*]u8 {
+    return &cartridge_buffer;
+}
+
 var gameboy: Gameboy = undefined;
 
-export fn start() void {
+export fn start(cartridge_length: u32) void {
+    const cartridge = Cartridge.init(cartridge_buffer[0..cartridge_length]);
+    gameboy.boot(cartridge);
+}
+
+export fn startWithTestCartridge() void {
     const romFile = @embedFile("blargg/03-op sp,hl.gb");
     var rom: [romFile.len]u8 = undefined;
     @memcpy(&rom, romFile);
